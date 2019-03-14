e5ed5cbc0ceb96b140eb8259a350c461ff89ecdd
github username <github email address>
2019-03-14 17:24:34 -0400

    Done.

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,1 +1,24 @@
 // Code DigitalClicker Component Here
+import React, { Component } from "react";
+
+class DigitalClicker extends Component {
+  state = {
+    timesClicked: 0
+  }
+
+  handleClick = () => {
+    this.setState(prevState => ({
+      timesClicked: prevState.timesClicked+1
+    }))
+  }
+
+  render() {
+    return (
+      <button type="button" onClick={this.handleClick}>
+        {this.state.timesClicked}
+      </button>
+    )
+  }
+} //end DigitalClicker
+
+export default DigitalClicker
--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -1,1 +1,42 @@
 // Code YouTubeDebugger Component Here
+import React, { Component } from "react";
+
+class YouTubeDebugger extends Component {
+  state = {
+    errors: [],
+    user: null,
+    settings: {
+      bitrate: 8,
+      video: {
+        resolution: "1080p"
+      }
+    }
+  }
+
+  changeBirate = () => {
+    this.setState({
+      settings: { ...this.state.settings, bitrate: 12}
+    })
+  }
+
+  changeResolution = () => {
+    this.setState({
+      settings: { ...this.state.settings, video: {
+        resolution: "720p"
+        }
+      }
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <button onClick={this.changeBirate} className="bitrate">Change Birate</button>
+        <button onClick={this.changeResolution} className="resolution">Change Resolution</button>
+      </div>
+    )
+  }
+
+}//end YouTubeDebugger class
+
+export default YouTubeDebugger
